74fe06df7c8d3c0706075247ffebc995a7e0a559
Gustav Svensk <gustav@axessions.com>
2020-03-10 22:24:45 +0100

    use formatservice

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Papa } from 'ngx-papaparse';
 import { BackendClientService } from '../../parser/backend-client.service';
+import { FormatService } from '../../parser/format-service/format.service';
 import { JSONstats } from 'src/app/parser/JSONstats.model';
 import { of } from 'rxjs';
 import { ScrollService } from 'src/app/shared/scroll.service';
@@ -18,9 +19,12 @@ export class UploadComponent implements OnInit {
   titlesConsumed: number;
   loading: boolean = false;
 
-  constructor(private papa: Papa,
+  constructor(
+    private papa: Papa,
     private backendClient: BackendClientService,
-    private scroller: ScrollService) { }
+    private scroller: ScrollService,
+    private formatter: FormatService
+    ) { }
 
   ngOnInit() {
   }
@@ -43,15 +47,15 @@ export class UploadComponent implements OnInit {
     this.titlesConsumed = content.length;
     this.load.emit(content.length);
     this.backendClient.titlesConsumed = of(content.length);
-    let json: Map<string, string> = new Map<string, string>();
+    const json: Map<string, string> = new Map<string, string>();
 
     content.forEach(item => {
       json[item[0]] = item[1]
     })
 
     this.backendClient.getResults(json).subscribe((data: JSONstats) => {
-      data = JSON.parse(data.toString());
       //console.log(data);
+      data = this.formatter.format(data);
       this.backendClient.stats = of(data);
       this.load.emit(null);
       this.upload.emit(null);
