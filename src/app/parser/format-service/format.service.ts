e5b3566e0f68f3c8ba942cfd1a2ecdf11ba13729
Gustav Svensk <gustav@axessions.com>
2020-03-10 22:24:03 +0100

    add stats format service

---
+++ src/app/parser/format-service/format.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { JSONstats } from '../JSONstats.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormatService {
+
+  constructor() { }
+
+  format(data: JSONstats) {
+    data.months.map(month => (month / 60).toFixed(1));
+    data.weekdays.map(weekday => (weekday / 60).toFixed(1));
+    data.totalTime = data.totalTime / 60
+    data.longestBinge = data.longestBinge / 60
+    data.mostWatchedSeriesTotalTime = data.mostWatchedSeriesTotalTime / 60
+    return data;
+  }
+}
