5e1dfd73124d92fe7f70238bd0ebd41ccdac2854
Gustav Svensk <gustav@axessions.com>
2020-04-25 17:03:31 +0200

    add new statistics model

---
+++ src/app/models/statistics.model.ts
@@ -0,0 +1,31 @@
+export interface Statistics {
+
+    general: {
+        titles: number;
+        totalTime: number;
+        longestBinge: number;
+        longestBingeDate: string;
+    }
+
+    movies: {
+        movies: number;
+        mostWatchedMovie: string;
+        mostWatchedMovieTimes: number;
+    }
+
+    series: {
+        mostWatchedSeries: string;
+        mostWatchedSeriesEpisodes: number;
+        mostWatchedSeriesTotalTime: number;
+    }
+
+    periods: {
+        weekdays: number[];
+        months: number[];
+        years: Object;
+    }
+
+    meta: {
+        notFound: number;
+    }
+}
