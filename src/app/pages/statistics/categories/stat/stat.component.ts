3e62a0ca5478cba363d1542eb4c1250f269727bf
Gustav Svensk <gustav@axessions.com>
2020-04-25 17:00:32 +0200

    add basic stat component

---
+++ src/app/pages/statistics/categories/stat/stat.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { StateService } from 'src/app/services/state/state.service';
+
+@Component({
+  selector: 'app-stat',
+  templateUrl: './stat.component.html',
+  styleUrls: ['./stat.component.css']
+})
+export class StatComponent implements OnInit {
+
+  @Input() number: number
+  @Input() name: string
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
