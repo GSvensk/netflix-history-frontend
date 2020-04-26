fb55e925c13add1160698aa744cc7428a4057cc7
Gustav Svensk <gustav@axessions.com>
2020-04-26 15:05:53 +0200

    update imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 import { ChartsModule } from 'ng2-charts';
 import { MatButtonModule } from '@angular/material/button';
-import { StatisticsComponent } from './statistics/statistics.component';
+import { StatisticsComponent } from './quickstats/statistics.component';
 import { InformationComponent } from './information/information.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { MatListModule } from '@angular/material/list';
@@ -14,10 +14,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { ServiceModule } from '../services/service.module';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SharedModule } from '../shared/shared.module';
-import { GeneralComponent } from './statistics/categories/general/general.component';
-import { StatComponent } from './statistics/categories/stat/stat.component';
-import { MoviesComponent } from './statistics/categories/movies/movies.component';
-import { SeriesComponent } from './statistics/categories/series/series.component';
+import { GeneralComponent } from './quickstats/categories/general/general.component';
+import { StatComponent } from './quickstats/categories/stat/stat.component';
+import { MoviesComponent } from './quickstats/categories/movies/movies.component';
+import { SeriesComponent } from './quickstats/categories/series/series.component';
+import { QuickstatsComponent } from './quickstats/quickstats.component';
 
 const routes: Routes = [
   { path: '', component: LandingpageComponent },
@@ -36,7 +37,8 @@ const routes: Routes = [
     GeneralComponent,
     StatComponent,
     MoviesComponent,
-    SeriesComponent
+    SeriesComponent,
+    QuickstatsComponent
   ],
   imports: [
     CommonModule,
