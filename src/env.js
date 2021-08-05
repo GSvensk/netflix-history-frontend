9b53fffec9f91ff715b23706d1de91aad1e8e488
Gustav Svensk <gustav.svensk95@gmail.com>
2021-08-05 13:14:52 +0200

    change apiUrl endpoint

--- src/env.js
+++ src/env.js
@@ -2,5 +2,5 @@
     window.__env = window.__env || {};
   
     // API url
-    window.__env.apiUrl = 'http://localhost:80';
+    window.__env.apiUrl = 'http://deplo-loadb-1hbvtifbbvdt2-0dae60e92af665d1.elb.eu-west-1.amazonaws.com:8080/';
   }(this));
