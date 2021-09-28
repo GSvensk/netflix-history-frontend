93089975353ac1414112416a1cc83f92a291443e
Gustav Svensk <gustav.svensk95@gmail.com>
2021-09-28 18:34:06 +0200

    Revert "change to drag and drop"
    This reverts commit 366fa78d0e91bb4b6a5ea36ace68f15157af1b70.

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ParseService } from '../../services/parse/parse.service';
 import { ScrollService } from 'src/app/shared/scroller/scroll.service';
 
@@ -11,8 +11,6 @@ export class UploadComponent implements OnInit {
   titlesConsumed: number;
   loading = false;
 
-  @ViewChild("upload") fileDropEl: ElementRef;
-
   constructor(
     private parseService: ParseService,
     private scroller: ScrollService
@@ -21,11 +19,6 @@ export class UploadComponent implements OnInit {
   ngOnInit() { }
 
   uploadFile($event) {
-    this.scroller.triggerScrollTo();
-    this.parseService.parse($event);
-  }
-
-   onFileDropped($event) {
     this.scroller.triggerScrollTo();
     this.parseService.parse($event);
   }
