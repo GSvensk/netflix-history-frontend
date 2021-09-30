6163541832dae15e393ac49525323d96fa3ae139
Gustav Svensk <gustav.svensk95@gmail.com>
2021-09-30 19:23:47 +0200

    switch tmdb logo to png

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NetflixActivity
+# MyNetflixHistory
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { FakeStatsButtonComponent } from './fake-stats-button/fake-stats-button';
+import { AttributionComponent } from './attribution/attribution.component';
 
 
 @NgModule({
-  declarations: [FakeStatsButtonComponent],
+  declarations: [FakeStatsButtonComponent, AttributionComponent],
   imports: [
     CommonModule,
     ScrollToModule.forRoot(),
   ],
   exports: [
-    FakeStatsButtonComponent
+    FakeStatsButtonComponent,
+    AttributionComponent
   ]
 })
 export class SharedModule { }
