7ac3ee0adeb7c6f5b6cc12367151b6b6fd5e6594
GSvensk <gustav.svensk95@gmail.com>
2022-04-11 19:30:24 +0200

    fix deploy dir

--- linode-deployment/deploy.sh
+++ linode-deployment/deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 HOST=${DEPLOY_USER:?"Deployment user is required"}@${SERVER:?"Server is required"}
 DEPLOYMENT_NAME=app_$(date +%Y%m%d_%H%M%S)
-DEPLOYMENT_DIR=/opt/mynetflixhistory/frontend/$DEPLOYMENT_NAME
+DEPLOYMENT_DIR=/usr/share/www/html
 
 ssh "$HOST" "mkdir -p $DEPLOYMENT_DIR"
-scp -r dist/netflix-activity $HOST:/usr/share/www/html
+scp -r dist/netflix-activity $HOST:$DEPLOYMENT_DIR
 scp nginx.conf $HOST:/etc/nginx/conf.d/default.conf
 
 ssh "$HOST" "sudo systemctl daemon-reload && sudo systemctl reload nginx"
