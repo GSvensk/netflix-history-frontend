b42d107ec0f611a72931d1260918387220455a6a
GSvensk <gustav.svensk95@gmail.com>
2022-04-12 21:55:38 +0200

    debug

--- linode-deployment/deploy.sh
+++ linode-deployment/deploy.sh
@@ -4,6 +4,10 @@ set -e
 HOST=${DEPLOY_USER:?"Deployment user is required"}@${SERVER:?"Server is required"}
 DEPLOYMENT_DIR=/usr/share/www/html
 
+ls
+ls dist/netflix-activity
+ls dist/netflix-activity/netflix-activity
+
 ssh "$HOST" "mkdir -p $DEPLOYMENT_DIR"
 scp -r dist/netflix-activity/ $HOST:$DEPLOYMENT_DIR
 scp nginx.conf $HOST:/etc/nginx/conf.d/default.conf
