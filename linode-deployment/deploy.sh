b2be790f419f3407ee17fd8f5ddca49754a04f8c
GSvensk <gustav.svensk95@gmail.com>
2022-04-12 22:02:04 +0200

    scp -r *

--- linode-deployment/deploy.sh
+++ linode-deployment/deploy.sh
@@ -5,7 +5,7 @@ HOST=${DEPLOY_USER:?"Deployment user is required"}@${SERVER:?"Server is required
 DEPLOYMENT_DIR=/usr/share/www/html
 
 ssh "$HOST" "mkdir -p $DEPLOYMENT_DIR"
-scp -r dist/netflix-activity/ $HOST:$DEPLOYMENT_DIR
+scp -r dist/netflix-activity/* $HOST:$DEPLOYMENT_DIR
 scp nginx.conf $HOST:/etc/nginx/conf.d/default.conf
 
 ssh "$HOST" "sudo systemctl daemon-reload && sudo systemctl reload nginx"
