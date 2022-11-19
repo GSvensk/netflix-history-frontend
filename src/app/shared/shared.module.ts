43eab71466e2fe9e1a9edbbcd305a64a899702a7
GSvensk <gustav.svensk95@gmail.com>
2022-11-19 17:17:48 +0100

    add disclaimer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { FakeStatsButtonComponent } from './fake-stats-button/fake-stats-button';
 import { AttributionComponent } from './attribution/attribution.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
 
 @NgModule({
-  declarations: [FakeStatsButtonComponent, AttributionComponent],
+  declarations: [FakeStatsButtonComponent, AttributionComponent, DisclaimerComponent],
   imports: [
     CommonModule,
     ScrollToModule.forRoot(),
   ],
   exports: [
     FakeStatsButtonComponent,
-    AttributionComponent
+    AttributionComponent,
+    DisclaimerComponent
   ]
 })
 export class SharedModule { }
